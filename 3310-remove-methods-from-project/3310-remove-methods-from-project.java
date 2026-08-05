3f1f89fef4cc4f9d61d52db619219717977e6b03
Shravanthi Dandam <23ag1a1216@gmail.com>
2026-08-05 13:50:06 +0530

    Time: 76 ms (54.81%), Space: 289.9 MB (45.19%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -0,0 +1,45 @@
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] inv) {
+         
+        List<List<Integer>> adj = new ArrayList<>(); 
+        for(int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>()); 
+        }
+
+        for(int i = 0; i < inv.length; i++) {
+            adj.get(inv[i][0]).add(inv[i][1]); 
+        }
+
+  
+        boolean vis[] = new boolean[n + 1]; 
+        dfs(adj, k, vis); 
+        boolean flag = true; 
+  
+        for(int i = 0; i < inv.length; i++) {
+            int u = inv[i][0], v = inv[i][1]; 
+            if(!vis[u]) {
+             
+                if(vis[v]) {
+                  
+                    flag = false; 
+                }
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>(); 
+        for(int i = 0; i < n; i++) {
+            if(flag && vis[i]) continue; 
+            ans.add(i);
+        }
+        return ans; 
+    }
+
+    private void dfs(List<List<Integer>> adj, int k, boolean[] vis) {
+        if(vis[k]) return; 
+
+        vis[k] = true; // mark visited 
+        for(Integer i: adj.get(k)) {
+            if(!vis[i]) dfs(adj, i, vis); 
+        }
+    }
+}
